52c839bca79ac71e616ff3b2a9fc29b2a9f4b06a
Pablo <96414596+psalazar5@users.noreply.github.com>
2023-07-31 15:43:34 -0400

    PokemonAPI

---
+++ PokemonAPI/PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    { //model is your db tables , DB is like excel spreadsheet. 
+        public int Id { get; set; } //almost like a column
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews  { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+    }
+}
