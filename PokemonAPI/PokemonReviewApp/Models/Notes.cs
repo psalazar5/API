7d547a0b299aafa6c20d04a0b7690a014eef6db3
Pablo <96414596+psalazar5@users.noreply.github.com>
2023-08-03 22:38:43 -0400

    API

---
+++ PokemonAPI/PokemonReviewApp/Models/Notes.cs
@@ -0,0 +1,29 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Notes
+    {
+    //-----------Creation of DataContext steps-------------//
+    //Creation of data context , bring in information to manipulate the database tables for quick access. 
+    //Setting DB to DataContext, setting many to many relationships to the database tables.
+
+    /*Then inside DataContext, when working with many-to-many tables, 
+     * we need to use on-model-creation for manipulation in order to customize the db without going into the db(OnModelCreation) and this is done when we're
+     * working with entity framework construction. 
+     * We need this customization in order so that people would understand what customizations I did to it. Manipulation of tables would be advantagous.
+     * Linkning entities displaying many to many relationships in entity while adding foreign and primary keys.*/
+
+    //------ Program File Setup------//
+
+    //After, we worked on program file setup, by adding our DBcontext<DataContext> we are using db builder service to bring in the SQL server into the program along with our connection string . 
+    //This allows us to reach into our app setting JSON & pulls out our connection string.
+
+    //----- Seeding DataBase ----//
+    /*After all this is completed, we seed our database, process of populating the database with intial data. Since I started the API it was empty, but since 
+     * seeding is crucial for this part of API creating, it insertss predefined data into PokemonAPI DB such as our properties that were created.
+     * Seeding this DB is done in the initialization phase , ensures our application has data to work with in the beginning. This is useful for testing and 
+     * development purposes as well providing a starting point for users who interact with the API.
+     */
+    /* After scripting 
+
+    }
+}
